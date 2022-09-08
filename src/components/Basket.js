48ac80e4ec15d919e8e90242bc3760c8940ca510
Sean Brown <seanbrown82@hotmail.com>
2022-09-08 23:06:51 +0100

    Feat: Add subtotal calculation to basket page. Style: Change basket p…age to 3 column layout. Remove add to basket button for basket items

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -1,9 +1,32 @@
 import BasketItem from "./BasketItem";
-import Row from "react-bootstrap/Row"
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import { useState, useEffect } from "react"
 
 const Basket = ( {basket, addToBasket, removeFromBasket, increaseQuantity, decreaseQuantity} ) => {
+    const [subtotal, setSubtotal] = useState(0);
+
+    useEffect(() => {
+
+        function calculateSubtotal() {
+            if(basket.length > 0) {
+
+                let newSubtotal = basket.map(item => item.quantity * item.price).reduce((acc, cur) => acc + cur)
+                setSubtotal(newSubtotal)
+            }
+        }
+
+        calculateSubtotal()
+        
+
+    }, [basket])
+
+
+
+
     return (
-        <Row md={2} className="p-5 g-3">
+        <Container className="p-5">
+            <Row md={3} className="border border-1 border-dark p-5 g-3 align-items-stretch">
             {basket.length > 0 ? basket.map(itemInBasket => {
                 return <BasketItem 
                 key={Math.random() * 100000} 
@@ -15,8 +38,13 @@ const Basket = ( {basket, addToBasket, removeFromBasket, increaseQuantity, decre
                 </BasketItem>
             })
             : <h2 className="text-center">No items in basket</h2>}
-        </Row>
+            </Row>
+            <h3>Subtotal: £{subtotal}</h3>
+        </Container>
+        
     )
 }
+
+
 
 export default Basket
--- src/components/BasketItem.js
+++ src/components/BasketItem.js
@@ -1,25 +1,27 @@
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import ButtonGroup from "react-bootstrap/ButtonGroup"
 
 
 const Product = ({ product, addToBasket, removeFromBasket, increaseQuantity, decreaseQuantity }) => {
     return (
         <Card className="product border-info border-3 border justify-content-evenly" id={product.id} >
             <Card.Body>
-                <Card.Img className="product-image" src={product.image}></Card.Img>
+                <Card.Img className="product-image" src={product.image} variant="top"></Card.Img>
                 <ul className="list-unstyled m-auto">
                     <li><Card.Text className="text-center fw-bold">{product.title}</Card.Text></li>
                     <li><Card.Text className="text-center ">£{(product.price * product.quantity).toFixed(2)}</Card.Text></li>
                 </ul>
-                <div className="quantity-control d-flex m-auto justify-content-center">
+            </Card.Body>
+            <Card.Footer>
+            <div className="quantity-control d-flex justify-content-center">
                     <button type="button" className="btn btn-secondary" onClick={decreaseQuantity}>-</button>
                     <input type="number" defaultValue={product.quantity}></input>
                     <button type="button" className="btn btn-secondary" onClick={increaseQuantity}>+</button>
                 </div>
-            </Card.Body>
-            <Button type="button" className="m-4" onClick={addToBasket}>Add to basket</Button>
-            <Button type="button" className="m-4 btn btn-danger" onClick={removeFromBasket} >Remove from basket </Button>
+            </Card.Footer>
+            <Button type="button" className="m-0 btn btn-danger" onClick={removeFromBasket} >Remove from basket </Button>
         </Card>
     )
 }
